7c3a98e1f324e538756f665fef3f322d72a285aa
NiNGia <ningia.coder@gmail.com>
2024-09-23 14:44:19 +0200

    feat(app): add footer and header components

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,102 +1,16 @@
-// import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
-// })
-// export class AppComponent {
-//   title = 'miniwedding';
-// }
-
-import { Component, ElementRef, ViewChild } from "@angular/core"
-import KeenSlider, { KeenSliderInstance } from "keen-slider"
-
-const WheelControls = (slider: any) => {
-  let touchTimeout: number | undefined
-  let position: { x: any; y: any }
-  let wheelActive: boolean
-
-  function dispatch(e: { deltaX: number; deltaY: number }, name: string) {
-    position.x -= e.deltaX
-    position.y -= e.deltaY
-    slider.container.dispatchEvent(
-      new CustomEvent(name, {
-        detail: {
-          x: position.x,
-          y: position.y,
-        },
-      })
-    )
-  }
-
-  function wheelStart(e:any) {
-    position = {
-      x: e.pageX,
-      y: e.pageY,
-    }
-    dispatch(e, "ksDragStart")
-  }
-
-  function wheel(e: any) {
-    dispatch(e, "ksDrag")
-  }
-
-  function wheelEnd(e: any) {
-    dispatch(e, "ksDragEnd")
-  }
-
-  function eventWheel(e: { preventDefault: () => void }) {
-    e.preventDefault()
-    if (!wheelActive) {
-      wheelStart(e)
-      wheelActive = true
-    }
-    wheel(e)
-    clearTimeout(touchTimeout)
-    touchTimeout = setTimeout(() => {
-      wheelActive = false
-      wheelEnd(e)
-    }, 50)
-  }
-
-  slider.on("created", () => {
-    slider.container.addEventListener("wheel", eventWheel, {
-      passive: false,
-    })
-  })
-}
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   styleUrls: [
     "./app.component.css",
   ],
 })
 export class AppComponent {
-  @ViewChild("sliderRef") sliderRef!: ElementRef<HTMLElement>;
-
-  slider!: KeenSliderInstance;
-
-  ngAfterViewInit() {
-    this.slider = new KeenSlider(
-      this.sliderRef?.nativeElement,
-      {
-        loop: false,
-        rubberband: false,
-        vertical: true,
-      },
-      [WheelControls]
-    )
-  }
-
-  ngOnDestroy() {
-    if (this.slider) this.slider.destroy()
-  }
 }
