0ce3bb189580cfd1ec0fae1c54380825df00e299
NiNGia <ningia.coder@gmail.com>
2024-09-28 01:08:34 +0200

    wip(home): add confirm button

--- web/src/app/pages/home/home.component.ts
+++ web/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ConfirmComponent } from '../../components/confirm/confirm.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ConfirmComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
