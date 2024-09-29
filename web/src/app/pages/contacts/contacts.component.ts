441c98d6656102ceef663bec8556b1cc179f8a7e
GiacobboNicola <ningia.coder@gmail.com>
2024-09-29 23:11:32 +0200

    feat(contact): complite page

--- web/src/app/pages/contacts/contacts.component.ts
+++ web/src/app/pages/contacts/contacts.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ConfirmComponent } from '../../components/confirm/confirm.component';
+import { SeparatorComponent } from '../../components/separator/separator.component';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, ConfirmComponent],
+  imports: [HeaderComponent, FooterComponent,SeparatorComponent, ConfirmComponent],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
