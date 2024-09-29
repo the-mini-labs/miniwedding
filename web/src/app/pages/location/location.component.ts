380df9a069890dd582fda959b359f0190491a1f2
GiacobboNicola <ningia.coder@gmail.com>
2024-09-29 18:15:10 +0200

    feat(location): first prototype

--- web/src/app/pages/location/location.component.ts
+++ web/src/app/pages/location/location.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ConfirmComponent } from '../../components/confirm/confirm.component';
+import { SeparatorComponent } from '../../components/separator/separator.component';
 
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, ConfirmComponent],
+  imports: [HeaderComponent, FooterComponent, ConfirmComponent, SeparatorComponent],
   templateUrl: './location.component.html',
   styleUrl: './location.component.css'
 })
