cf7e6c62638944edd8693459cfa4cf49389d6b63
GiacobboNicola <ningia.coder@gmail.com>
2024-09-29 18:44:50 +0200

    feat(weedinglist): complited first trail

--- web/src/app/pages/wedding-list/wedding-list.component.ts
+++ web/src/app/pages/wedding-list/wedding-list.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ConfirmComponent } from '../../components/confirm/confirm.component';
+import { SeparatorComponent } from '../../components/separator/separator.component';
 
 @Component({
   selector: 'app-wedding-list',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent, ConfirmComponent],
+  imports: [HeaderComponent,FooterComponent, ConfirmComponent, SeparatorComponent],
   templateUrl: './wedding-list.component.html',
   styleUrl: './wedding-list.component.css'
 })
