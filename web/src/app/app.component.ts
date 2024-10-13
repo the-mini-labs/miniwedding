6e7e2fb85d271cdf1a93b76ab05bac98fecc0a70
GiacobboNicola <ningia.coder@gmail.com>
2024-10-13 15:13:59 +0200

    refact: SLA

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ConfirmComponent } from './components/confirm/confirm.component';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ConfirmComponent],
   styleUrls: [
     "./app.component.css",
   ],
